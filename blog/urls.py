8af991aefe8ee1a8029be373db2a8b26dc17e670
jungmin86 <jmkimm0p@gmail.com>
2024-03-29 12:56:00 +0900

    Add publish and delete feature

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+    path("post/<int:id>/", views.post_detail, name="post_detail"),
+    path("post/new", views.post_new, name="post_new"),
+    path("post/<int:id>/edit/", views.post_edit, name="post_edit"),
+    path(r"^drafts/$", views.post_draft_list, name="post_draft_list"),
+    path(r"^post/(?P<id>\d+)/publish/$", views.post_publish, name="post_publish"),
+    path(r"^post/(?P<id>\d+)/remove/$", views.post_remove, name="post_remove"),
+]
---
+++ blog/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
+
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", id=post.id)
+    else:
+        form = PostForm()
+    return render(request, "blog/post_edit.html", {"form": form, "is_editing": False})
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, "blog/post_edit.html", {"form": form, "is_editing": True})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
+    return render(request, "blog/post_draft_list.html", {"posts": posts})
+
+
+def post_publish(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.publish()
+    return redirect("post_detail", id=id)
+
+
+def post_remove(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    return redirect("post_list")
